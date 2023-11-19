16044b59d274fa6503773cba7804e40441f1d6c4
vaishnavikuber <150253479+vaishnavikuber@users.noreply.github.com>
2023-11-19 20:08:57 +0530

    Add files via upload

---
+++ NestedIfElseDemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.daytwo;
+
+public class NestedIfElseDemo {
+
+	public static void main(String[] args) {
+		int a=40, b=10, c=5;
+		System.out.println("The largest number is");
+		if (a > b) {
+			if (a > c)
+				System.out.println(a);
+			else
+				System.out.println(c);
+		} else {
+			if (c > b)
+				System.out.println(c);
+			else
+				System.out.println(b);
+		}
+
+	}
+
+}
