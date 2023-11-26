83eab52f1ac8a7a32dd437276816ebeda2eb47d5
vaishnavikuber <150253479+vaishnavikuber@users.noreply.github.com>
2023-11-26 19:22:31 +0530

    GreetClass.java

--- dayeight/interfaces/functionalinterfaces/GreetClass.java
+++ dayeight/interfaces/functionalinterfaces/GreetClass.java
@@ -1,1 +1,12 @@
 
+//Program to demonstrate FunctionalInterface - Child Class
+package com.tnsif.dayeight.interfaces.functionalinterfaces;
+
+public class GreetClass implements GreetInterface {
+
+	@Override
+	public String greet() {		
+		return "Welcome to the world of Java";
+	}
+
+}
